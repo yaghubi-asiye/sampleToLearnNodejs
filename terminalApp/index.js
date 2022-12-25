a72d9e86f616215c4108b0c29a4225e1ced3b434
yaghoubi <yaghoubi@persiatc.com>
2022-12-25 12:10:53 +0330

    yargs package: user yargs for create terminal app

--- terminalApp/index.js
+++ terminalApp/index.js
@@ -9,15 +9,53 @@
 // console.log(typeof data);
 
 
-const command = process.argv[2];
-switch(command) {
-    case 'add':
-        console.log('adding ...')
-        break;
-    case 'remove':
-        console.log('removeing ...')
-        break;
-    default:
-        console.log('command not valid ...')
-        break;
-}
+
+// const command = process.argv[2];
+// switch(command) {
+//     case 'add':
+//         console.log('adding ...')
+//         break;
+//     case 'remove':
+//         console.log('removeing ...')
+//         break;
+//     default:
+//         console.log('command not valid ...')
+//         break;
+// }
+
+
+const yargs = require("yargs");
+// console.log(process.argv)
+// console.log(yargs.argv)
+
+yargs.command({
+    command: "create",
+    describe: "[create new person]",
+    aliases: ["c"],
+    builder: {
+        fullname: {
+            alias: "f",
+            describe: "Persons Fullname",
+            demandOption: true,
+            type: "string",
+        },
+        phone: {
+            alias: "p",
+            describe: "Person phone number",
+            demandOption: true,
+            type: "number",
+        },
+        email: {
+            alias: "e",
+            describe: "Person email address",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler({fullname, phone, email}) {
+        console.log(fullname, email, phone)
+    },
+});
+
+yargs.parse();
+// console.log(yargs.argv)
