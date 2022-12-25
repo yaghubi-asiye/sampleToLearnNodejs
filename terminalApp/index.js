023bf182166c4584769afdec807542dbf77d91f7
yaghoubi <yaghoubi@persiatc.com>
2022-12-25 11:26:50 +0330

    first app/get command and show message

---
+++ terminalApp/index.js
@@ -0,0 +1,23 @@
+// const fs = require('fs');
+// const persons = [
+//     {id:1, name:'asiye'},
+//     {id:2, name:'zahra'},
+//     {id:3, name:'maryam'}
+// ];
+// fs.writeFileSync('db/person.json', JSON.stringify(persons));
+// const data = JSON.parse(fs.readFileSync('db/person.json').toString());
+// console.log(typeof data);
+
+
+const command = process.argv[2];
+switch(command) {
+    case 'add':
+        console.log('adding ...')
+        break;
+    case 'remove':
+        console.log('removeing ...')
+        break;
+    default:
+        console.log('command not valid ...')
+        break;
+}
